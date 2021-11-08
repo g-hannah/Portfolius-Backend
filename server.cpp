58aacea154ef884a0397cb9f998554a10e775264
g-hannah <g.amh.86@outlook.fr>
2021-11-08 15:35:43 +0000

    fork child process to handle client request

--- server.cpp
+++ server.cpp
@@ -1,5 +1,7 @@
 #include "server.h"
 
+#define BUFFER_SIZE 2048
+
 Server::Server()
 {
 	this->listening_socket = new SocketObj();
@@ -17,11 +19,81 @@ void Server::run()
 
 	while (1)	
 	{
-		struct sockaddr_in *sock = this->listening_socket->wait_for_client_request();
-		if (NULL == sock)
+		int client_socket = this->listening_socket->wait_for_client_request();
+		if (STDERR_FILENO >= client_socket)
 			continue;
 
-		ExchangeRatesManager manager = ExchangeRatesManager::instance();
-		manager->handle_client_request(sock);
+		pid_t pid = fork();
+		if (0 > pid)
+		{
+			throw std::exception("Failed to fork new process");
+		}
+		else
+		if (0 == pid)
+		{
+			// child process executes this block
+			ExchangeRatesManager manager = ExchangeRatesManager::instance();
+			char *buffer = malloc(BUFFER_SIZE);
+			assert(NULL != buffer);
+
+			/*
+			 * Should receive a JSON-encoded request
+			 * from the client, such as:
+			 *
+			 * {
+					"type" : "rate",
+					"currency" : "BTC"
+			   }
+
+				OR
+
+				{
+					"type" : "historic",
+					"currency" : [
+						"BTC",
+						"ETH"
+					]
+				}
+			 */
+			read(client_socket, buffer, BUFFER_SIZE);
+
+			/*
+				TODO
+
+				Use external json lib to parse request,
+				obtain the required data from the
+				exchange rates manager and send a JSON
+				encoded response to the client.
+
+
+				e.g.,
+
+				JsonObj jsonobj = Json::read_data(buffer);
+				std::string type = jsonobj->get_node("type");
+				if (IS_SINGLE_RATE_REQUEST(type))
+					// get the rate
+				else
+					// get array of rates
+
+				JsonObj outjson = new JsonObj;
+				outjson.put("type", type);
+				outjson.put("data", Json::encode_data(the_data));
+				std::string stringified = outjson.get_string();
+
+				size_t sent_len = send(client_socket, stringified, stringified.length());
+				if (stringified.length() != sent_len)
+					throw std::exception("Failed to send response to client");
+
+				exit(EXIT_SUCCESS);
+			 */
+
+			// child process has to exit
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			// parent process executes this block
+			continue;
+		}
 	}
 }
