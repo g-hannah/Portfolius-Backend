e91134740875ec3f81c2f5c5f28ee759e99b4b69
g-hannah <g.amh.86@outlook.fr>
2021-11-08 15:09:45 +0000

    define listen() and wait_for_client_request() methods

--- socketobj.cpp
+++ socketobj.cpp
@@ -6,11 +6,28 @@
 SocketObj::SocketObj()
 {
 	this->sock = -1;
-}
+/*
+	this->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	struct sockaddr_in sin;
 
+	clear_struct(&sin);
+
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(port);
+
+	if (bind(this->sock, std::static_cast<struct sockaddr *>(&sin), sizeof(sin)))
+		throw std::exception("Failed to bind socket");
+*/
+}
 
 SocketObj::~SocketObj()
 {
+}
+
+void SocketObj::set_port_no(const uint16_t port)
+{
+	this->port = port;
 }
 
 bool SocketObj::connect()
@@ -26,6 +43,47 @@ bool SocketObj::connect()
 
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(this->port);
+}
+
+uint16_t SocketObj::get_port_no()
+{
+	return this->port_no;
+}
+
+void SocketObj::listen()
+{
+	if ((this->sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) < 0)
+		throw std::exception("Failed to create socket");
+
+	struct sockaddr_in sin;
+
+	clear_struct(&sin);
+
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(port);
+
+	if (bind(this->sock, std::static_cast<struct sockaddr *>(&sin), sizeof(sin)))
+		throw std::exception("Failed to bind socket");
+
+	if (listen(this->sock, this->value_backlog) < 0)
+		throw std::exception("Failed to listen on socket");
+}
+
+struct sockaddr *SocketObj::wait_for_client_request()
+{
+	int client_socket = -1;
+	struct sockaddr_in *sin = NULL;
+	socklen_t socklen = 0;
+
+	sin = malloc(sizeof(struct sockaddr_in));
+	assert(NULL != sin);
+
+	client_socket = accept(this->sock, std::static_cast<struct sockaddr *>(sin), &socklen);
+	if (0 > client_socket)
+		throw std::exception("Error accepting client request");
+
+	return sin;
 }
 
 /*
