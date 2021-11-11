4fb9b4004b056ff8f20368bb178ec58549979597
g-hannah <g.amh.86@outlook.fr>
2021-11-11 22:23:08 +0000

    add new methods; wrap in namespace portfolius

--- server.h
+++ server.h
@@ -4,30 +4,45 @@
 #include <iostream.h>
 #include "<unistd.h>"
 #include "socketobj.h"
+#include "rapidjson/document.h"
 
 #define LISTENING_PORT	34567
+
+#define REQUEST_SUCCESS	0x01
+#define REQUEST_ERROR	0xff
+
+#define REQUEST_TYPE_SINGLE_RATE	"rate"
+#define REQUEST_TYPE_HISTORIC		"historic"
 
 /**
  * A singleton class for running the server
  *
  * @author Gary Hannah
  */
-class Server
+namespace portfolius
 {
-	public:
-		static Server *instance()
-		{
-			if (!server_)
-				server_ = new Server
+	class Server
+	{
+		public:
+			static Server *instance()
+			{
+				if (!server_)
+					server_ = new Server
 
-			return server_
-		}
+				return server_
+			}
 
-		void run(std::string);
+			void run(std::string);
 
-	private:
-		static Server server_ = 0;
-		ListeningSocket *listening_socket = 0; // owned pointer
+		private:
+			static Server server_ = 0;
+			ListeningSocket *listening_socket = 0; // owned pointer
+
+			void client_error(portfolius::Client&, std::string);
+			void send_response(portfolius::Client&, std::vector<Rate*>);
+			bool _is_valid_type(std::string);
+			bool _is_valid_currency(std::string);
+	};
 }
 
 #endif
