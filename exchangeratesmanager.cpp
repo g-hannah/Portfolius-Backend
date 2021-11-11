9ddbcec9aa98fe658e57b45f7f6209fe81a95fc3
g-hannah <g.amh.86@outlook.fr>
2021-11-11 22:27:18 +0000

    use secondary map while getting fresh rates

--- exchangeratesmanager.cpp
+++ exchangeratesmanager.cpp
@@ -10,151 +10,220 @@
 
 ExchangeRatesManager::ExchangeRatesManager()
 {
+	try
+	{
+		this->_map_primary = this->_read_rates();
+	/*
+	 * std::map's copy ctor will copy everything
+	 */
+		this->_map_secondary = this->_map_primary;
+	}
+	catch (std::exception e)
+	{
+		std::cerr << "ERM ctor: Failed to read exchange rates: " << e.what() << std::endl;
+	}
 }
 
 ExchangeRatesManager::~ExchangeRatesManager()
 {
-	if (this->_sock)
-		delete this->_sock;
 }
 
 void ExchangeRatesManager::start()
 {
-	this->init_mutex.lock();
+/*
+ * We only want this method to be running
+ * once by one thread. So place this here
+ * in the event that somehow start() was
+ * called twice.
+ */
+	this->running_mutex.lock();
 
-	if (this->_initialised)
+	if (this->_running)
 	{
-		this->init_mutex.unlock();
+		this->running_mutex.unlock();
 		return;
 	}
 
-	this->_initialised = true;
-	this->init_mutex.unlock();
+	this->running = true;
+	this->running_mutex.unlock();
 
-	ApplicationSettings *settings = ApplicationSettings::instance();
+	portfolius::ApplicationSettings *settings = portfolius::ApplicationSettings::instance();
 
 	while (true)
 	{
-		char **currencies = settings->get_currencies();
-
-		if (currencies)
+		if (!this->_map_secondary.empty())
 		{
-			for (int i = 0; currencies[i]; ++i)
+			std::map<std::string,std::vector<Rate>>::iterator iter = this->_map_secondary.begin();
+
+			while (iter != this->_map_secondary.end())
 			{
-				std::string = PATH_TO_DATA + "/" + currencies[i] + ".json";
-				FileHandler fh = new FileHandler(PATH_TO_DATA);
-			//	std::string json = this->read_rates(PATH_TO_DATA);
+				std::string key = iter->first;
+				std::vector<Rate> vec = iter->second;
 
-
-				std::string json;
-
-				try
-				{
-					json = fh.read_all();
-				}
-				catch (std::exception e)
-				{
-					continue;
-				}
-
-				rapidjson::Document d;
-				d.Parse(json);
-				rapidjson::Value& v = d["data"];
-
-				if (!v.IsArray()) // it should be...
-					continue;
-
-				//std::vector<Rate> vector_rates;
-
-				//for (rapidjson::SizeType i = 0, n = v.Size(); i < n; ++i)
-				//	vector_rates.push_back(new Rate(v[i]["timestamp"], v[i]["value"]));
-
-				std::string api_url = API_ENDPOINT API_CRYPTO_ARG + "=" + currencies[i] + "&" + API_CURRENCY_ARG + "=GBP";
-
-			/*
-			 * Let the API think we are a web browser
-			 * (some servers will ignore anything that isn't)
-			 */
+				/*
+				 * Let the API think we are a web browser
+				 * (some servers will ignore anything that isn't)
+				 */
 				httplib::Headers headers = {
 					{ "User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0" },
 					{ "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" }
 				};
 
 				httplib::SSLClient client(API_ENDPOINT, headers);
-				auto result = client.Get("/data/price?" + API_CRYPTO_ARG + "=" + currencies[i] + "&" + API_CURRENCY_ARG + "=GBP");
+				auto result = client.Get("/data/price?" + API_CRYPTO_ARG + "=" + key + "&" + API_CURRENCY_ARG + "=GBP");
 
 				std::string body = result->body;
 
+			/*
+			 * Response from API is of format:
+			 *
+			 * { "GBP" : <value> }
+			 */
 				rapidjson::Document d2;
 				d2.Parse(body);
 				rapidjson::Value& v2 = d2["GBP"];
-				double rate_value = v2.GetDouble();
 
-				std::time_t now = std::chrono::system_clock::now();
+				double fresh_rate = v2.GetDouble();
+				std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-				//Rate new_rate = new Rate((long)now, rate_value);
-				//vector_rates.push_back(new_rate);
-
-
-			/*
-			 * Add new object to the d["data"] array
-			 * with new rate:
-			 *
-			 * { "timetamp" : <timestamp>, "value" : <value> }
-			 */
-				rapidjson::Document d3;
-
-				d3.setObject();
-				rapidjson::Value object(rapidjson::kObjectType);
-				object.AddMember("timestamp", now);
-				object.AddMember("value", rate_value);
-				d.PushBack(object, d3.GetAllocator());
-
-				this->write_rates(d);
+				vec.push_back(new Rate(now, fresh_rate));
 			}
 		}
 
-		/*
-		 * Retrieve rates from the endpoint
-		 *
-		 *	- parse JSON data
-		 *
-		 * Add to data structure
-		 *
-		 *	- Acquire mutex that protects the data structure
-		 *	- A hashmap (currency_key -> list_of_data)
-		 *	- Unlock mutex
-		 *
-		 * Write to disk
-		 *
-		 */
+		this->rates_mutex.lock();
+
+	/*
+	 * Copy over updated map
+	 */
+		this->_map_primary = this->_map_secondary;
+		this->_write_rates();
+
+		this->rates_mutex.unlock();
+
 		sleep(SLEEP_TIME);
 	}
 }
 
-std::string ExchangeRatesManager::read_rates(std::string path)
+/**
+ * Reads rates into map
+ */
+std::map<std::string,std::vector<Rate*>> ExchangeRatesManager::_read_rates()
 {
-	std::string s = "nothing";
-	return s;
+	ApplicationSettings *settings = ApplicationSettings::instance();
+	char **currencies = settings->get_currencies();
+
+	if (!currencies)
+		return nullptr;
+
+	std::map<std::string,std::vector<Rate>> map;
+
+	for (int i = 0; currencies[i]; ++i)
+	{
+		std::string path = PATH_TO_DATA + "/" + currencies[i] + ".json";
+		FileHandler fh = new FileHandler(path);
+		std::string json = fh.read_all();
+
+		rapidjson::Document d;
+
+		d.Parse(json);
+		rapidjson::Value& v = d["data"];
+
+		assert(v.IsArray());
+
+		std::vector<Rate> vec;
+
+		for (rapidjson::SizeType k, n = v.Size(); k < n; ++k)
+		{
+			rapidjson::Value& v_ts = v[i]["timestamp"];
+			rapidjson::Value& v_val = v[i]["value"];
+
+			vec.push_back(new Rate(v_ts.GetDouble(), v_val.GetDouble()));
+		}
+
+		std::string key = currencies[i];
+		map[key] = vec;
+	}
+
+	return map;
 }
 
-void ExchangeRatesManager::write_rates(rapidjson::Document d)
+
+/**
+ * Writes rates for each currency to its respective file.
+ *
+ * e.g.,
+ *
+ * BTC.json
+ * ETH.json
+ */
+void ExchangeRatesManager::_write_rates()
 {
+	std::map<std::string,std::vector<Rate*>> map = this->_primary_map;
+	std::map<std::string,std::vector<Rate*>>::iterator iter = map.begin();
+
+	while (iter != map.end())
+	{
+		std::string key = iter->first;
+		std::vector<Rate*> vec = iter->second;
+
+		rapidjson::Document d;
+		rapidjson::Document::AllocatorType& allocator = d.GetAllocator();
+		d.SetObject();
+		rapidjson::Value arr(rapidjson::kArrayType);
+
+		for (int i = 0, n = vec.size(); i < n; ++i)
+		{
+			rapidjson::Value val(rapidjson::kObjectType);
+			val.AddMember("timestamp", vec[i]->get_timestamp());
+			val.AddMember("value", vec[i]->get_value());
+			arr.PushBack(val, allocator):
+		}
+
+		std::string path = PATH_TO_DATA + "/" + key + ".json";
+
+/*
+		rapidjson::StringBuffer buffer;
+		rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+		d.Accept(writer);
+
+		std::string json = buffer.GetString();
+*/
+
+		char *buffer = (char *)calloc(FILESTREAMWRITER_BUFSIZE);
+		assert(buffer);
+
+		FILE *fp = fopen(path, "w");
+		rapidjson::FileWriteStream os(fp, buffer, FILESTREAMWRITER_BUFSIZE);
+		rapidjson::Writer<rapidjson::FileWriteStream> out_writer(os);
+
+	/*
+	 * This automatically writes the JSON to the file
+	 */
+		d.Accept(out_writer);
+
+		fclose(fp);
+		fp = nullptr;
+
+		free(buffer);
+		buffer = nullptr;
+
+		++iter;
+	}
 }
 
 /**
  * This method can be called from another thread, so
  * a mutex to protect the data structures is necessary
  */
-double ExchangeRatesManager::getRateForCurrency(std::string currency)
+Rate& ExchangeRatesManager::get_rate_for_currency(std::string currency)
 {
 	this->rates_mutex.lock();
 /*
  * Critical section
  */
 
-	std::map<std::string,std::vector<Rate>> rates = this->map_rates;
-	std::vector<Rate> vec = rates.get(currency);
+	std::map<std::string,std::vector<Rate*>> rates = this->_primary_map;
+	std::vector<Rate*> vec = rates.get(currency);
 	if (!vec)
 	{
 		this->rates_mutex.unlock();
@@ -162,39 +231,34 @@ double ExchangeRatesManager::getRateForCurrency(std::string currency)
 	}
 
 
-	Rate rate = vec[vec.size()-1];
+	Rate *rate = vec[vec.size()-1];
 
 	this->rates_mutex.unlock();
 
-	return rate.get_value();
+	Rate& r = rate;
+	return r;
 }
 
 /**
  * This method can be called from another thread, so
  * a mutex to protect the data structures is necessary
  */
-std::list<double> ExchangeRatesManager::getRatesHistoryForCurrency(std::string)
+std::vector<Rate*> ExchangeRatesManager::get_rates_history_for_currency(std::string)
 {
 	this->rates_mutex.lock();
 /*
  * Critical section
  */
 
-	std::map<std::string,std::vector<Rate>> rates = this->map_rates;
-	std::vector<Rate> vec = rates.get(currency);
+	std::map<std::string,std::vector<Rate*>> rates = this->_primary_map;
+	std::vector<Rate*> vec = rates.get(currency);
 	if (!vec)
 	{
 		this->rates_mutex.unlock();
 		throw std::exception("No rates for currency \"" + currency + "\"");
 	}
 
-	std::size_t num = std::min(vec.size(), std::static_cast<std::size_t>(24));
-	std::list list;
-
-	for (std::size_t i = num-1; i >= 0; --i)
-		list.push_back(vec[i]);
-
 	this->rates_mutex.unlock();
 
-	return list;
+	return vec;
 }
