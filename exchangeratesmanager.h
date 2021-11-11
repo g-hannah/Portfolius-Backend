36605e804890169852adeb70851da270c1fe2a2f
g-hannah <g.amh.86@outlook.fr>
2021-11-11 22:26:39 +0000

    add second map; add mutexes

--- exchangeratesmanager.h
+++ exchangeratesmanager.h
@@ -3,11 +3,22 @@
 
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
+#include <assert.h>
 #include "httplib/httplib.h"
 #include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/memorystream.h"
+#include <vector>
 #include <mutex>
+#include <ctime>
+#include <chrono>
+#include <cstdio>
 
 #define API_ENDPOINT ""
+
+#define FILESTREAMWRITER_BUFSIZE	32768
 
 /**
  * A singleton class to get exchange rates
@@ -15,37 +26,53 @@
  *
  * @author Gary Hannah
  */
-class ExchangeRatesManager
+namespace portfolius
 {
-	public:
+	class ExchangeRatesManager
+	{
+		public:
 
-		static ExchangeRatesManager *instance()
-		{
-			if (!_instance)
-				_instance = new ExchangeRatesManager();
+			static ExchangeRatesManager *instance()
+			{
+				if (!_instance)
+					_instance = new ExchangeRatesManager();
 
-			return _instance;
-		}
+				return _instance;
+			}
 
-		void start();
-		double getRateForCurrency(std::string);
-		std::list<double> getRatesHistoryForCurrency(std::string);
+			void start();
+			Rate& get_rate_for_currency(std::string);
+			std::vector<Rate*> get_rates_history_for_currency(std::string);
 
-	private:
+		private:
 
-		static ExchangeRatesManager *_instance = 0;
-		static bool _initialised = false;
-		ExchangeRatesManager();
-		virtual ~ExchangeRatesManager();
+			static ExchangeRatesManager *_instance = 0;
+			ExchangeRatesManager();
+			virtual ~ExchangeRatesManager();
 
-		void write_rates(rapidjson::Document);
-		std::string read_rates(std::string);
+			bool _running = false;
+			void write_rates(rapidjson::Document);
+			std::string read_rates(std::string);
 
-		SocketObj *_sock = 0;
-
-		std::map<std::string,std::vector<Rate>> map_rates;
-		std::mutex rates_mutex;
-		std::mutex init_mutex;
+		/*
+		 * When we are iterating through the map's keys in order
+		 * to get fresh rates from the API and add the fresh rates
+		 * to each vector, we don't want to have the mutex locked
+		 * for the entire time the rates are being updated because
+		 * if a client request comes within that time to get rates,
+		 * the latency could be high since we are waiting for a
+		 * response from the API for each currency before the
+		 * mutex would be unlocked.
+		 *
+		 * So have two maps. This secondary map will be updated
+		 * when getting fresh rates. Once we are done, lock the
+		 * mutex for the primary one and copy over the data.
+		 */
+			std::map<std::string,std::vector<Rate*>> _map_secondary;
+			std::map<std::string,std::vector<Rate*>> _map_primary;
+			std::mutex rates_mutex;
+			std::mutex running_mutex;
+	};
 }
 
 
