a732e0066cbcc6bfe214b462f0cfbd6e018da33f
g-hannah <g.amh.86@outlook.fr>
2021-11-13 11:28:51 +0000

    add missing namespace specifiers

--- rate.cpp
+++ rate.cpp
@@ -1,11 +1,11 @@
 #include "rate.h"
 
-std::time_t Rate::get_timestamp()
+std::time_t portfolius::Rate::get_timestamp()
 {
 	return this->timestamp;
 }
 
-double Rate::get_value()
+double portfolius::Rate::get_value()
 {
 	return this->value;
 }
