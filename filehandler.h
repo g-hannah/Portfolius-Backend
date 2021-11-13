7a283db77774887afa8c4b5d2ed279e2ab65aec0
g-hannah <g.amh.86@outlook.fr>
2021-11-13 11:26:34 +0000

    add missing header files

--- filehandler.h
+++ filehandler.h
@@ -1,8 +1,12 @@
 #ifndef __FILE_HANDLER_h__
 #define __FILE_HANDLER_h__ 1
 
+#include <assert.h>
+#include <string>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <stdexcept>
 
 namespace portfolius
 {
