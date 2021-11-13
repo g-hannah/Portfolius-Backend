f79142635a1d179e2a0264724ed8514aa788845d
g-hannah <g.amh.86@outlook.fr>
2021-11-13 11:29:21 +0000

    add methods; add missing header files; define error types

--- server.h
+++ server.h
@@ -1,16 +1,23 @@
 #ifndef __SERVER_h__
 #define __SERVER_h__ 1
 
-#include <iostream.h>
-#include "<unistd.h>"
+#include <iostream>
 #include <cstddef>
-#include "socketobj.h"
+#include <unistd.h>
+#include <vector>
+#include "exchangeratesmanager.h"
+#include "listeningsocket.h"
 #include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include "applicationsettings.h"
+#include "client.h"
+#include "rate.h"
 
 #define LISTENING_PORT	34567
 
-#define REQUEST_SUCCESS	0x01
-#define REQUEST_ERROR	0xff
+#define CLIENT_ERROR_INVALID_TYPE		0x01
+#define CLIENT_ERROR_INVALID_CURRENCY	0x02
 
 #define REQUEST_TYPE_SINGLE_RATE	"rate"
 #define REQUEST_TYPE_HISTORIC		"historic"
@@ -30,19 +37,22 @@ namespace portfolius
 			static Server *instance()
 			{
 				if (!_instance)
-					_instance = new Server
+					_instance = new Server;
 
 				return _instance;
 			}
 
-			void run(std::string);
+			void run();
 
 		private:
-			static Server _instance = 0;
-			ListeningSocket *listening_socket = 0; // owned pointer
+			Server();
+			virtual ~Server();
 
-			void client_error(portfolius::Client&, std::string);
-			void send_response(portfolius::Client&, std::vector<Rate*>);
+			static Server *_instance;
+			portfolius::ListeningSocket *listening_socket = 0;
+
+			void client_error(portfolius::Client *, int);
+			void send_response(portfolius::Client *, std::vector<portfolius::Rate*>);
 			bool _is_valid_type(std::string);
 			bool _is_valid_currency(std::string);
 	};
