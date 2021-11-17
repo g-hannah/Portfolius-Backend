3235e157d762623ce691091facb821e7dcf9f5fc
g-hannah <g.amh.86@outlook.fr>
2021-11-17 19:09:36 +0000

    declare copy ctors / assigment copy ctors

--- rate.h
+++ rate.h
@@ -13,13 +13,19 @@ namespace portfolius
 	class Rate
 	{
 		public:
-			Rate(std::time_t t, double v) : timestamp(t), value(v) {}
+
+			explicit Rate(std::time_t t, double v) : timestamp(t), value(v) {}
+			Rate(const Rate& other);
+			Rate(Rate&& other); // rvalue copy ctor
+			Rate& operator=(Rate& other);
+			Rate& operator=(Rate&& other); // rvalue ref copy assignment ctor
 			virtual ~Rate() {}
 
 			std::time_t get_timestamp();
 			double get_value();
 
 		private:
+
 			std::time_t timestamp;
 			double value;
 	};
