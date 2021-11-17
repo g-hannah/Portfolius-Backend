7759a5edeae5745dbafd816a7efd041571d2a21a
g-hannah <g.amh.86@outlook.fr>
2021-11-17 19:09:46 +0000

    define copy ctors / assignment copy ctors

--- rate.cpp
+++ rate.cpp
@@ -1,5 +1,42 @@
 #include "rate.h"
 
+/**
+ * Copy ctor
+ */
+portfolius::Rate::Rate(const portfolius::Rate &other)
+{
+	this->timestamp = other.timestamp;
+	this->value = other.value;
+}
+
+/**
+ * Move ctor
+ */
+portfolius::Rate::Rate(portfolius::Rate &&other)
+{
+	this->timestamp = other.timestamp;
+	this->value = other.value;
+}
+
+/**
+ * Copy assignment operator overload
+ */
+portfolius::Rate portfolius::Rate::operator=(portfolius::Rate other)
+{
+	this->timestamp = other.timestamp;
+	this->value = other.value;
+}
+
+/**
+ * Move semantics assignment operator overload
+ */
+portfolius::Rate& portfolius::Rate::operator=(portfolius::Rate &&other)
+{
+	this->timestamp = other.timestamp;
+	this->value = other.value;
+	return *this;
+}
+
 std::time_t portfolius::Rate::get_timestamp()
 {
 	return this->timestamp;
